8dd34883cbb287c496603b172daa964c6626644e
joaocarlosa <joaocarloslima99@gmail.com>
2023-09-19 02:52:18 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+
+    <nav class="grey darken-4">
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo" style="padding-left: 20px;">Tray</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><router-link class="text-white text-lg hover:underline" to="/">Home</router-link></li>
+          <li><router-link class="text-white text-lg hover:underline" to="/register">Register</router-link></li>
+          <li><router-link class="text-white text-lg hover:underline" to="/login">Login</router-link></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="content-wrapper">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.content-wrapper {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 64px);
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 
 createApp(App).use(store).use(router).mount('#app')
