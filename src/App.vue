e1823595ccf566092472f66b5a30acb35da6d6f0
joaocarlosa <joaocarloslima99@gmail.com>
2023-09-19 16:13:09 -0300

    feat: exibe links se o usuario estiver autenticado

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-
     <nav class="grey darken-4">
       <div class="nav-wrapper">
         <a href="#" class="brand-logo" style="padding-left: 20px;">Tray</a>
@@ -13,18 +12,44 @@
     </nav>
 
     <div class="content-wrapper">
+
+      <div v-if="authToken">
+        <!-- Lista de links -->
+        <div class="row mt-5">
+          <div class="col s4">
+            <router-link to="/sale">Vendas</router-link>
+          </div>
+          <div class="col s4">
+            <router-link to="/seller">Vendedores</router-link>
+          </div>
+          <div class="col s4">
+            <router-link to="/email">Email</router-link>
+          </div>
+        </div>
+      </div>
       <router-view />
     </div>
   </div>
 </template>
 
+<script lang="ts">
+export default {
+  data() {
+    return {
+      authToken: null,
+    };
+  },
+  mounted() {
+    this.authToken = window.sessionStorage.getItem('authToken');
+  },
+};
+</script>
+
 <style>
 .content-wrapper {
   margin-top: 20px;
   width: 80%;
-  padding: 0 10%; /* Adicione o espaçamento nas laterais usando padding */
-  text-align: center; /* Centraliza o conteúdo horizontalmente */
+  padding: 0 10%;
+  text-align: center;
 }
-
-
 </style>
